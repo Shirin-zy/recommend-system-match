4864ae5b489fad7d92615f7425f871d276713188
zhangyu <zhangyu05@xiaoduotech.com>
2025-03-16 15:57:19 +0800

    feat: 侧边栏+状态管理

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,139 +1,71 @@
 import { Outlet, Link } from "umi";
 import styles from "./index.less";
-import { Menu, Breadcrumb, Flex } from "antd";
-import {
-  SettingOutlined,
-  BarChartOutlined,
-  BankOutlined,
-  TeamOutlined,
-  DownOutlined,
-  UpOutlined,
-  HomeOutlined,
-  UserOutlined,
-} from "@ant-design/icons";
+import { TeamOutlined, DownOutlined, UpOutlined } from "@ant-design/icons";
 import { useState } from "react";
+import { Provider, observer } from "mobx-react";
+import { App, AppStore } from "@/stores/app";
+import Side from "./components/side";
+import MyBreadcrumb from "./components/breadcrumb";
+import { Button } from "antd";
+import { noop } from "antd/es/_util/warning";
 
-const BreadcrumbRoutes = [
-  {
-    href: "/home",
-    title: <HomeOutlined />,
-  },
-  {
-    href: "/home",
-    title: (
-      <>
-        <UserOutlined />
-        <span>主页</span>
-      </>
-    ),
-  },
-  {
-    href: "/setting",
-    title: (
-      <>
-        <SettingOutlined />
-        <span>设置</span>
-      </>
-    ),
-  },
-];
-
-const orerMenu = [
-  {
-    name: "主页",
-    router: "/",
-    icon: <BankOutlined />,
-    children: [
-      { name: "主页", router: "/home" },
-      { name: "数据概览", router: "/option-2" },
-    ],
-  },
-  {
-    name: "数据概览",
-    router: "/navigation-two",
-    icon: <BarChartOutlined />,
-  },
-  {
-    name: "设置",
-    router: "/",
-    icon: <SettingOutlined />,
-    children: [
-      { name: "设置", router: "/setting" },
-      { name: "登录", router: "/login" },
-    ],
-  },
-];
-
-const Layout = () => {
-  const user_name = "root账户";
+const Layout = observer(() => {
   const [showUserInfo, setShowUserInfo] = useState<boolean>(false);
-
   return (
-    <div>
-      {/* 顶部导航菜单 */}
-      <nav className={styles.navs}>
-        <img src="https://www.cqut.edu.cn/2024/img/logo.png" alt="" />
-        <div className={styles.user_info}>
-          <TeamOutlined style={{ color: "#ffffff", fontSize: 18 }} />
-          <span style={{ color: "#ffffff", fontSize: 16, fontWeight: 300 }}>
-            {user_name}
-          </span>
-          <div onClick={() => setShowUserInfo(!showUserInfo)}>
-            {showUserInfo ? (
-              <UpOutlined style={{ color: "#ffffff", fontSize: 14 }} />
-            ) : (
-              <DownOutlined style={{ color: "#ffffff", fontSize: 14 }} />
-            )}
+    <Provider App={AppStore}>
+      <div>
+        {/* 顶部导航菜单 */}
+        <nav className={styles.navs}>
+          <img src="https://www.cqut.edu.cn/2024/img/logo.png" alt="" />
+          <div className={styles.user_info}>
+            <TeamOutlined style={{ color: "#ffffff", fontSize: 18 }} />
+            <span style={{ color: "#ffffff", fontSize: 16, fontWeight: 300 }}>
+              {App.userName}
+            </span>
+            <div onClick={() => setShowUserInfo(!showUserInfo)}>
+              {showUserInfo ? (
+                <UpOutlined style={{ color: "#ffffff", fontSize: 14 }} />
+              ) : (
+                <DownOutlined style={{ color: "#ffffff", fontSize: 14 }} />
+              )}
+            </div>
           </div>
-        </div>
-        <div
-          className={`${styles.detail} ${showUserInfo ? styles.show : styles.hidden}`}
-        >
-          <div>当前账号</div>
-          <div></div>
-        </div>
-      </nav>
-      {/* 主体 */}
-      <div className={styles.body}>
-        <div className={styles.side}>
-          <Menu
-            mode="inline"
-            style={{ lineHeight: "50px" }}
-            defaultSelectedKeys={["0"]}
-            theme="light"
+          <div
+            className={`${styles.detail} ${showUserInfo ? styles.show : styles.hidden}`}
           >
-            {orerMenu.map((item, index) => {
-              if (item.children && item.children.length > 0) {
-                return (
-                  <Menu.SubMenu key={index} icon={item.icon} title={item.name}>
-                    {item.children.map((child, childIndex) => (
-                      <Menu.Item key={`${index}-${childIndex}`}>
-                        <Link to={child.router}>{child.name}</Link>
-                      </Menu.Item>
-                    ))}
-                  </Menu.SubMenu>
-                );
-              } else {
-                return (
-                  <Menu.Item key={index} icon={item.icon}>
-                    <Link to={item.router}>{item.name}</Link>
-                  </Menu.Item>
-                );
-              }
-            })}
-          </Menu>
-        </div>
-        <div className={styles.body}>
-          <div className={styles.breadcrumb}>
-            <Breadcrumb style={{ fontSize: 16 }} items={BreadcrumbRoutes} />
+            <div>当前账号</div>
+            <div>
+              <Button
+                onClick={() => {
+                  App.setName("admin");
+                }}
+              >
+                reset
+              </Button>
+            </div>
           </div>
-          <div className={styles.content}>
-            <Outlet />
+        </nav>
+        {/* 主体 */}
+        <div className={styles.body}>
+          <div className={styles.side}>
+            <Side
+              addBreadcrumbRoutes={(href: string, title: JSX.Element) => {
+                App.pushRoutes({ href: href, title: title });
+              }}
+            />
+          </div>
+          <div className={styles.body}>
+            <div className={styles.breadcrumb}>
+              <MyBreadcrumb routes={App.routes} />
+            </div>
+            <div className={styles.content}>
+              <Outlet />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Provider>
   );
-};
+});
 
 export default Layout;
